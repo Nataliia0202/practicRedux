14de72234e52f2f448f54ed663d1899c9d288b73
Nataliia Svystula <natashasvistula@gmail.com>
2023-01-03 15:46:57 +0200

    add

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,7 +1,9 @@
 import { MdClose } from 'react-icons/md';
 import css from './Task.module.css';
+import { deleteTask, toggleCompleted } from 'redux/tasksSlice';
+
 import { useDispatch } from 'react-redux';
-import { deleteTask, toggleCompleted } from 'redux/actions';
+
 export const Task = ({ task }) => {
     const dispatch = useDispatch();
     const handleDelete = () => dispatch(deleteTask(task.id));
